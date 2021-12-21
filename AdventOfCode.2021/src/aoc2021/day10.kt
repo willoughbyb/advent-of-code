bcd9ec24cccd3f03e1e6ce35c3fe8c30b5770a02
Brandon Willoughby <willoughbyb@protonmail.com>
2021-12-20 18:47:20 -0700

    day10 P2

--- AdventOfCode.2021/src/aoc2021/day10.kt
+++ AdventOfCode.2021/src/aoc2021/day10.kt
@@ -1,6 +1,7 @@
 package aoc2021
 
 import java.util.*
+import kotlin.math.floor
 
 data class Scanner(val input: String, var start: Int = 0, var current: Int = 0) {
     fun next(): Char {
@@ -16,8 +17,7 @@ val openers = charArrayOf('(', '[', '{', '<')
 val closers = charArrayOf(')', ']', '}', '>')
 val points = intArrayOf(3, 57, 1197, 25137)
 
-fun checkCorruption(line: String): Int {
-//    println("Checking $line")
+fun checkCorruption(line: String): Long? {
     val scanner = Scanner(line)
 
     var currentChunkIndex: Int = -1
@@ -36,24 +36,31 @@ fun checkCorruption(line: String): Int {
                         openers.indexOf(chunks.peek())
                     } else -1
                 } else {
-//                    println("  Corrupted Chunk: found $ch, expected ${closers[currentChunkIndex]}")
-                    return points[closers.indexOf(ch)]
+                    return null // -points[closers.indexOf(ch)]
                 }
             }
         }
     } while (scanner.hasNext())
 
     if (chunks.size > 0) {
-//        println("Chunk is unclosed: $chunks")
+        println("Chunk is unclosed: $chunks")
+        // get closing list
+        val closingList = chunks.map { closers[openers.indexOf(it)] }.joinToString("")
+        val closingListValue = closingList.toCharArray()
+            .fold(0L) { sum, e -> (sum * 5) + (closers.indexOf(e) + 1) }
+        println("  to close: $closingListValue -> $closingList")
+
+        return closingListValue
     }
     return 0
 }
 
 fun main() {
     val input = AOCInputReader.readDay(10)
-    val valids = input.groupBy { line -> checkCorruption(line) }
-        .mapValues { it.value.size }
-        .toList()
-        .fold(0) { sum, element -> sum + element.first * element.second }
+    val valids = input.mapNotNull { checkCorruption(it) }
+        .sorted()
     println(valids)
+
+    val middleIndex = floor(valids.size / 2.0).toInt()
+    println("$middleIndex -> ${valids[middleIndex]}")
 }
